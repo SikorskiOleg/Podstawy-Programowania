ab2e770dab8c8e50a557323b77f7eba35e2275e0
Oleg Sikorski <o.sikorski.935@studms.ug.edu.pl>
2025-03-07 13:13:59 +0100

    zapisz

---
+++ Zadanie 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, h, l; // r - radius, h - height, l - length
+            Console.WriteLine("Podaj promień podstawy: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj wysokość stozka: ");
+            h = Convert.ToDouble(Console.ReadLine());
+            l = Math.Sqrt(r * r + h * h);
+            Console.WriteLine("Pole powierzchni stozka: {0}", Math.PI * r * (r + l));
+            Console.WriteLine("Objętość stozka: {0}", Math.PI * r * r * h / 3);
+            Console.ReadKey();
+        }
+    }
+}
