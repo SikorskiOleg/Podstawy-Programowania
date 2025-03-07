a6abf449aafd5aff0baca2dc1b1c8ac2a21ba1a8
Oleg Sikorski <o.sikorski.935@studms.ug.edu.pl>
2025-03-07 14:01:17 +0100

    zapisz

---
+++ Zadanie 9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b; // a - first number, b - second number
+            Console.Write("Podaj liczbę a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj liczbę b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Suma kwadratów a  i b to {0}", a*a + b*b);
+            Console.ReadKey();
+        }
+    }
+}
