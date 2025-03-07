000a9528beccc7d5675e99d96155aa36789d2b31
Oleg Sikorski <o.sikorski.935@studms.ug.edu.pl>
2025-03-07 13:52:01 +0100

    Koniec gałęzi

---
+++ Zadanie 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b; // a - first number, b - second number
+            Console.Write("Podaj liczbę a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj liczbę b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Suma: {0}", a + b);
+            Console.WriteLine("Różnica: {0}", a - b);
+            Console.WriteLine("Iloczyn: {0}", a * b);
+            Console.WriteLine("Iloraz: {0}", a / b);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Zadanie 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b; // a - first number, b - second number
+            Console.Write("Podaj liczbę a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj liczbę b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("a/b = {0} r{1}",a/b, a%b );
+            Console.ReadKey();
+        }
+    }
+}
