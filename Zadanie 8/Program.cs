0f6250365faf2ba73f5a260551012ee69cffb174
Oleg Sikorski <o.sikorski.935@studms.ug.edu.pl>
2025-03-07 13:57:47 +0100

    zapisz

---
+++ Zadanie 8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Dany jest ciag: an = 3*n-1");
+            Console.Write("Podaj n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("a{0} = {1}", n, 3 * n - 1);
+            Console.ReadKey();
+        }
+    }
+}
