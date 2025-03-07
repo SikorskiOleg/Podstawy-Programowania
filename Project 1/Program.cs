371067b9e1475175dd4b0443240f8a70dcad9881
Oleg Sikorski <o.sikorski.935@studms.ug.edu.pl>
2025-03-07 14:17:53 +0100

    zapisz

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -10,6 +10,18 @@ namespace Project_1
     {
         static void Main(string[] args)
         {
+            int a;
+            Console.WriteLine("Enter a number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ Zadanie 2(Instrukcje warunkowe)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2_Instrukcje_warunkowe_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Enter a number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a > 0)
+            {
+                Console.WriteLine("The number is greater than 0");
+            }
+            else
+            {
+                if (a == 0)
+                {
+                    Console.WriteLine("The number is equal to 0");
+                }
+                else
+                {
+                    Console.WriteLine("The number is smaller than 0");
+                }
+
+
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
