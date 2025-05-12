981d72f14a3dcae99b7d97b12564ee0491475ce3
Oleg Sikorski <o.sikorski.935@studms.ug.edu.pl>
2025-05-12 14:11:19 +0200

    zapis

---
+++ Zadanie 4 (Funkcje-i-procedury)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4__Funkcje_i_procedury_
+{
+    class Program
+    {
+        static int ile_cyfr(int liczba)
+        {
+            int i = 0;
+            while(liczba / 10 > 0)
+            {
+                liczba /= 10;
+                i++;
+            }
+            return i + 1;
+
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("W funkcji jest {0} liczb.", ile_cyfr(0));
+            Console.WriteLine("W funkcji jest {0} liczb.", ile_cyfr(3453423));
+            Console.WriteLine("W funkcji jest {0} liczb.", ile_cyfr(1234));
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Zadanie 5 (Funkcje-i-procedury)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5__Funkcje_i_procedury_
+{
+    class Program
+    {
+        static int pierwsza(int liczba)
+        {
+            for(int i=2; i<liczba;i++)
+            {
+                if (liczba % i == 0) return 0;
+            }
+            return 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Liczba jest liczbą pierwszą dla wartości 1 i nie pierwszą dla wartości 0");
+            Console.WriteLine("Liczba 7: {0}", pierwsza(7));
+            Console.WriteLine("Liczba 28: {0}", pierwsza(28));
+            Console.WriteLine("Liczba 57: {0}", pierwsza(57));
+            Console.ReadKey(true);
+        }
+    }
+}
