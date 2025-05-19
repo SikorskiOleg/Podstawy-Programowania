d0ffbdfbab5253740a1ffc737d667ebd62fe57fe
Oleg Sikorski <o.sikorski.935@studms.ug.edu.pl>
2025-05-19 09:01:34 +0200

    zapis

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -8,8 +8,22 @@ namespace Project_1
 {
     class Program
     {
+        struct Osoba
+        {
+            public string imie, nazwisko;
+            public int rok_urodzenia;
+        }
         static void Main(string[] args)
         {
+            Osoba o;
+            Console.WriteLine("Podaj imię: ");
+            o.imie = Console.ReadLine();
+            Console.WriteLine("Podaj nazwisko: ");
+            o.nazwisko = Console.ReadLine();
+            Console.WriteLine("Podaj rok urodzenia: ");
+            o.rok_urodzenia = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} {1} rok urodzenia {2}", o.imie,o.nazwisko,o.rok_urodzenia);
+            Console.ReadKey(true);
         }
     }
 }
---
+++ Zadanie 2 (Procedury C#)/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2__Procedury_C__
+{
+    class Program
+    {
+        struct Uczen
+        {
+            public string imie, nazwisko;
+            public byte[] ocena;
+        }
+        static void Main(string[] args)
+        {
+            Uczen[] uczniowie;
+            int ilu_uczniow, ile_ocen, i,j; //zmienne pomocnicze
+            //wczytuje liczbe uczniow
+            Console.Write("Podaj ilość uczniów w klasie: ");
+            ilu_uczniow = Convert.ToInt32(Console.ReadLine());
+            //przydziela pamięć do tablicy struktur
+            uczniowie = new Uczen[ilu_uczniow];
+            //wczytuje dane uczniow od uzytkownika
+            for (i = 0; i < ilu_uczniow; i++)
+            {
+                Console.Write("Podaj imię ucznia {0}: ", i + 1);
+                uczniowie[i].imie = Console.ReadLine();
+                Console.Write("Podaj imię nazwisko {0}: ", i + 1);
+                uczniowie[i].nazwisko = Console.ReadLine();
+                //wczytuje liczbe ocen ucznia
+                Console.Write("Ile ocen ma: {0} {1}: ", uczniowie[i].imie, uczniowie[i].nazwisko);
+                ile_ocen = Convert.ToInt32(Console.ReadLine());
+                //przydzielam pamięć dla talbicy ocen
+                uczniowie[i].ocena = new byte[ile_ocen];     
+                //wczytuje oceny ucznia
+                for (j=0; j < ile_ocen; j++)
+                {
+                    Console.WriteLine("Podaj ocenę {0}: ", i + 1);
+                    uczniowie[i].ocena[j] = Convert.ToByte(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            //wypisuje dane uczniów na ekranie
+            for (i=0; i < ilu_uczniow; i++)
+            {
+                Console.Write("{0}.{1} {2}: ", i + 1, uczniowie[i].imie, uczniowie[i].nazwisko);
+                foreach (byte o in uczniowie[i].ocena)
+                    Console.Write("{0}, ", o);
+                Console.WriteLine("");//przejscie do nowego wiersza
+            }
+            Console.ReadKey(true);
+        }           
+    }
+}
